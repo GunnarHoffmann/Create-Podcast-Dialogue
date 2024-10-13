267ab5fe87b083969079e76c61fe4c4c21b9b789
GunnarHoffmann <47139778+GunnarHoffmann@users.noreply.github.com>
2024-10-14 00:57:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,7 +85,7 @@ def concatenate_audios(file_list, output_file):
 # Processing the input by concatenating "_END"
 if user_input:
     # Iterate through the text array and synthesize audio based on index
-    inputarray = json.loads(text_input)
+    inputarray = json.loads(user_input)
     for index, text in enumerate(inputarray):
         if index % 2 == 0:
             # Even index, use Speaker 1
